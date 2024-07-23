7084804d5b0dde04f50e6716e3418609f8e6e1a9
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-07-23 00:54:41 -0400

    Completed the details of the invitation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const welcomeMessage = document.getElementById("welcome-message");
     const inviteMessage = document.getElementById("invite-message");
     const mainContent = document.getElementById("main-content");
+    // const main = document.getElementById("Invitation-part");
 
     // Set up the transition for main content
     mainContent.style.transition = "opacity 1s ease-in-out";
@@ -12,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // After 1 second, fade to the invite message
     setTimeout(() => {
         welcomeMessage.classList.remove("visible");
-
+        
         setTimeout(() => {
             welcomeMessage.style.display = "none"; // Hide welcome message completely
             inviteMessage.classList.add("visible");
@@ -23,14 +24,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 // Show the main content with the fade-in effect
                 mainContent.style.display = "auto"; // Ensure it's visible
-                mainContent.classList.add("visible");
-                document.body.style.overflow = "scroll"; // Allow scrolling only on the main content
-                document.body.style.height = "100%"
+                // myLetter.classList.add("visible");
+                document.body.style.overflow = "auto";
+                // myLetter.style.overflow = "scroll";
+                
                 mainContent.style.opacity = "1";
-                setTimeout(() => {
-                    inviteMessage.style.display = "none"; // Hide invite message completely
-                }, 1000); // Shorter delay to match the fade-out effect
-            }, 1000); // Shorter delay to match the fade-out effect
+                const elements = document.querySelectorAll('.main');
+
+                                // Iterate over each element and set its opacity to 1
+                                elements.forEach(element => {
+                                    element.style.opacity = '1';
+                                });
+            }, 1400); // Shorter delay to match the fade-out effect
+        }, 1500); 
         }, 1000); // Shorter delay to match the fade-out effect
-    }, 1000); // Shorter delay to match the fade-out effect
+    // }, 1000); // Shorter delay to match the fade-out effect
 });
