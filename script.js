5639c463129af0d633ca85e0cb5c61710e76cc64
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-07-25 00:19:31 -0400

    Hopefully final version

--- script.js
+++ script.js
@@ -24,11 +24,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 // Show the main content with the fade-in effect
                 mainContent.style.display = "auto"; // Ensure it's visible
+                
                 // myLetter.classList.add("visible");
                 document.body.style.overflow = "auto";
                 // myLetter.style.overflow = "scroll";
                 
                 mainContent.style.opacity = "1";
+                mainContent.style.background = 'url("download.png") no-repeat center center fixed';
+                mainContent.style.backgroundSize = '110%';
                 const elements = document.querySelectorAll('.main');
 
                                 // Iterate over each element and set its opacity to 1
@@ -38,5 +41,4 @@ document.addEventListener("DOMContentLoaded", () => {
             }, 1400); // Shorter delay to match the fade-out effect
         }, 1500); 
         }, 1000); // Shorter delay to match the fade-out effect
-    // }, 1000); // Shorter delay to match the fade-out effect
 });
